9147cef2aee6feefbf8155ab9dd8032bb9377727
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-29 12:18:00 +0530

    Content updated with link

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
+
+  const [mainData, setMainData] = useState([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
+      const response = await res.json();
+
+      if (response) {
+        setMainData(response);
+      }
+    }
+
+    fetchData();
+
+  }, []);
+
   return (
-    <div>Contact</div>
+    <div>Contact Page
+      {mainData?.map(({ id, title, body }) => (
+        <Link to={`/contact/${id}`}>
+          <div>
+            <h3>{title}</h3>
+            <h3>{body}</h3>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
