d8adbd55f830a279cf94acec4686c56f993cbacc
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-29 13:37:32 +0530

    Content updated

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { flushSync } from 'react-dom';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Contact = () => {
 
   const [mainData, setMainData] = useState([]);
+  const [theme, setTheme] = useState(false);
+
+  console.log(theme);
 
   useEffect(() => {
 
@@ -22,11 +26,14 @@ const Contact = () => {
   }, []);
 
   return (
-    <ContactContainer>Contact Page
+    <ContactContainer>
+      Contact Page
+      <button onClick={() => { setTheme(prev => (prev ? false : true)) }}>Change Theme</button>
       <ContactBlock>
         {mainData?.map(({ id, title, body }) => (
           <Link to={`/contact/${id}`}>
-            <ContactBlockUnt>
+            <ContactBlockUnt changeTheme={theme}>
+              <img src="https://t4.ftcdn.net/jpg/02/10/96/95/360_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg" alt='image' />
               <h3>{title}</h3>
               {/* <h3>{body}</h3> */}
             </ContactBlockUnt>
@@ -50,19 +57,28 @@ const ContactContainer = styled.main`
 const ContactBlock = styled.div`
   width: 90%;
   display: grid;
-  grid-template-columns: repeat(3,1fr);
+  grid-template-columns: repeat(4,1fr);
   grid-template-rows: auto;
   gap: 25px;
 `;
 
 const ContactBlockUnt = styled.div`
-  background-color: bisque;
+  background-color: ${(props) => (props.changeTheme ? "black" : "bisque")};
   padding: 20px;
   border-radius: 7px;
-  border: 1px solid black;
+  border: 2px solid ${(props) => (props.changeTheme ? "bisque" : "black")};
   transition: all 400ms ease-in;
 
   &:hover{
     background-color: inherit;
+  }
+
+  h3{
+    font-size: 16px;
+  }
+
+  img{
+    width: 100%;
+    object-fit: contain;
   }
 `;
