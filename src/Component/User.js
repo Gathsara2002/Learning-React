ed20ab4a7dcf3bab41aaef5910dbf49eac662e4f
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-29 11:51:41 +0530

    useParams task

--- src/Component/User.js
+++ src/Component/User.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const User = () => {
 
+    const [data, setData] = useState({});
+
     const { id } = useParams();
+
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            const response = await res.json();
+            if(response){
+                setData(response);
+            }
+        }
+
+        getData();
+
+        return ()=>{
+            getData();
+        }
+
+    }, [id]);
+
     console.log(id);
 
     return (
-        <div>User</div>
+        <div>User
+            <h3>{data.id}</h3>
+            <h3>{data.title}</h3>
+            <h3>{data.body}</h3>
+        </div>
     )
 }
 
